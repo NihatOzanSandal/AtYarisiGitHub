ae307462eee27fdd1fe30aab85309e378cd797ca
Nihat Ozan SANDAL <nihat.sandal@std.medipol.edu.tr>
2022-04-12 23:30:40 +0300

    Login ekranininda denemeler

--- At_Yarisi/Controllers/HomeController.cs
+++ At_Yarisi/Controllers/HomeController.cs
@@ -5,29 +5,49 @@ using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
 using At_Yarisi.Models.siniflar;
+using System.Web.Security;
 
 namespace At_Yarisi.Controllers
 {
     public class HomeController : Controller
     {
-        
-
-        
+        private CONTEXT db = new CONTEXT();
         public ActionResult LoginPage()
         {
             ViewBag.Message = "Login Page";
 
             return View();
         }
-    
+        [HttpPost]
+        /*
+         //c.admins first or default ne oluyor onu öğren düzenle
+         
+        public ActionResult Login(Members ad)
+        {
+            //var bilgiler = CONTEXT.Members.FirstOrDefault(x => x.Kullanici == ad.Kullanici && x.Sifre == ad.Sifre);
+
+            var bilgiler =Members.FirstOrDefault(x => x.Kullanici == ad.Kullanici && x.Sifre == ad.Sifre);
+
+            if (bilgiler != null)
+            {
+
+                FormsAuthentication.SetAuthCookie(bilgiler.Kullanici, false);
+                Session["Kullanici"] = bilgiler.Kullanici.ToString();
+                return RedirectToAction("Index", "Admin");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        */
         public ActionResult GetStartedPage()
         {
             ViewBag.Message = "Get Started Page ";
 
             return View();
         }
-        //Kullanıcı başlangıç login işlemi seçme ekranı ->
-        
+        //Kullanıcı başlangıç login işlemi seçme ekranı ->       
         public ActionResult CreateAccountPage()
         {
             ViewBag.Message = "Create Account Page ";
@@ -58,15 +78,12 @@ namespace At_Yarisi.Controllers
 
             return View();
         }
-
-         public ActionResult RulesPage()
+        public ActionResult RulesPage()
         {
             ViewBag.Message = "Rules Page";
 
             return View();
         }
-        
-
 
     }
 }
