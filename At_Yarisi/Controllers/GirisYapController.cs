d52c87eff3bff4df9d87ce428cb2bb03a760350f
Nihat Ozan SANDAL <nihat.sandal@std.medipol.edu.tr>
2022-04-17 21:24:48 +0300

    Login ekranı çaılışır hala getirildi

---
+++ At_Yarisi/Controllers/GirisYapController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using At_Yarisi.Models.siniflar;
+
+namespace At_Yarisi.Controllers
+{
+    public class GirisYapController : Controller
+    {
+        // GET: GirisYap
+        CONTEXT c = new CONTEXT();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Members ad)
+        {
+            var bilgiler = c.Members.FirstOrDefault(x => x.Email == ad.Email && x.Password == ad.Password);
+            if (bilgiler != null)
+            {
+
+                FormsAuthentication.SetAuthCookie(bilgiler.Email, false);
+                Session["Email"] = bilgiler.Email.ToString();
+                return RedirectToAction("MainMenuPage", "Main");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
--- At_Yarisi/Controllers/HomeController.cs
+++ At_Yarisi/Controllers/HomeController.cs
@@ -11,43 +11,13 @@ namespace At_Yarisi.Controllers
 {
     public class HomeController : Controller
     {
-        private CONTEXT db = new CONTEXT();
-        public ActionResult LoginPage()
-        {
-            ViewBag.Message = "Login Page";
-
-            return View();
-        }
-        [HttpPost]
-        /*
-         //c.admins first or default ne oluyor onu öğren düzenle
-         
-        public ActionResult Login(Members ad)
-        {
-            //var bilgiler = CONTEXT.Members.FirstOrDefault(x => x.Kullanici == ad.Kullanici && x.Sifre == ad.Sifre);
-
-            var bilgiler =Members.FirstOrDefault(x => x.Kullanici == ad.Kullanici && x.Sifre == ad.Sifre);
-
-            if (bilgiler != null)
-            {
-
-                FormsAuthentication.SetAuthCookie(bilgiler.Kullanici, false);
-                Session["Kullanici"] = bilgiler.Kullanici.ToString();
-                return RedirectToAction("Index", "Admin");
-            }
-            else
-            {
-                return View();
-            }
-        }
-        */
+        
         public ActionResult GetStartedPage()
         {
             ViewBag.Message = "Get Started Page ";
 
             return View();
-        }
-        //Kullanıcı başlangıç login işlemi seçme ekranı ->       
+        }       
         public ActionResult CreateAccountPage()
         {
             ViewBag.Message = "Create Account Page ";
