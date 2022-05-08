688b5d38c951022da48faaf42c0bd92449cfa96a
Nihat Ozan SANDAL <nihat.sandal@std.medipol.edu.tr>
2022-05-08 11:42:01 +0300

    cüzdan ekranları tamamlandı

--- At_Yarisi/Controllers/WalletController.cs
+++ At_Yarisi/Controllers/WalletController.cs
@@ -22,7 +22,29 @@ namespace At_Yarisi.Controllers
         {
             return View();
         }
+
+        //*************************************************************
+
+
+
         public ActionResult AddPaymentMethod()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddPaymentMethod(FormCollection form)
+        {
+            return View();
+        }
+
+
+
+        //*************************************************************
+        public ActionResult WithdrawYourMoney()
+        {
+            return View();
+        }
+        public ActionResult DeleteChangePaymentMethod()
         {
             return View();
         }
