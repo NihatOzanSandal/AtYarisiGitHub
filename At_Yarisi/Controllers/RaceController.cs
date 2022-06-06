7beb7d1aeb9bed855cdc38d4c303e3ed3659f16e
Nihat Ozan SANDAL <nihat.sandal@std.medipol.edu.tr>
2022-06-06 14:59:47 +0300

    race db yle bağlanıyor güncelleme öncesi

--- At_Yarisi/Controllers/RaceController.cs
+++ At_Yarisi/Controllers/RaceController.cs
@@ -17,46 +17,45 @@ namespace At_Yarisi.Controllers
         }
         public ActionResult Race()
         {
-            
-                var bilgi = db.Horse.Where(x => x.HorseName != "BoşIsim");
+            Session["MevcutYarisId"] = db.Bets.Max(q => q.ID);
+            Convert.ToInt16(Session["MevcutYarisId"]);
+
+            var bilgi = db.Horse.Where(x => x.HorseName != "BoşIsim");
                 return View(bilgi.ToList());
                     
         }
         [HttpPost]
-        public ActionResult Race(Horse model)
+        public ActionResult Race(Bets model)
         {
            
-            var bilgiler = db.Horse.FirstOrDefault(x => x.HorseName == model.HorseName);
+            var bilgiler = db.Bets.FirstOrDefault(x => x.MemberId == model.MemberId);
             
-
             if (bilgiler != null)
             {
-                Session["HorseName"] = bilgiler.HorseName;
-                Session["HorseRatio"] = bilgiler.HorseRatio;
-                Session["HorseNumber"] = bilgiler.HorseNumber;
+                model.RaceId += 1;
+                db.Bets.Add(model);
+                db.SaveChanges();
+
                 //db.Horse.Add(model);
                 //db.SaveChanges();
                 return View();
             }
             else
             {
-                Response.Write("<script lang='JavaScript'>alert('Lütfen Girdiğiniz Bilgileri Kontrol Edip Tekrar Deneyin');</script>");
+                Response.Write("<script lang='JavaScript'>alert('Member Id Almada Sorun Yaşandı Tekrar Login Olunuz ');</script>");
                 return View();
             }
         }
         public ActionResult PastRacesPage(int id)
         {
-            //SORU BİR BU KOŞULA UYAN BÜTÜN SORGULARI NASIL ÇEKERİM
 
             var bilgi = db.Bets.Where(x => x.MemberId == id);
-            //var bilgi = db.Bets.Find(x => x.MemberId == id);
             if (bilgi != null)
             {
                 //Session["AmountOfBet"] = bilgi.AmountOfBet;
                 //Session["HorseId"] = bilgi.HorseId;
                 //Session["EarningAmount"] = bilgi.EarningAmount;
 
-                //SORU 2 BURDA BETS YERİNE BİLGİ Mİ KULLANILMALI
                 return View(bilgi.ToList());
             }
             else
@@ -64,11 +63,18 @@ namespace At_Yarisi.Controllers
                 Response.Write("<script lang='JavaScript'>alert('Geçmiş Yarışınız Bulunmamakta');</script>");
                 return View("SetMain");
             }
+        } 
+        /*Kullanılmayanlar -> */
+        public ActionResult HorseRaceJs()
+        {    
+            return View();
+
         }
         public ActionResult HorseRace()
         {    
             return View();
 
         }
+       
     }
 }
